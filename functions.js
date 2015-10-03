2b07bcbc3a5f722f5c959bd63fc50f5bfd9dcabf
Rompei <rompei0924@gmail.com>
2015-10-03 21:29:42 +0900

    Fix functions.

--- functions.js
+++ functions.js
@@ -1,28 +1,28 @@
 'use strict';
 
-module.exports = function(args){
-  this.moveForward = function(drone, time, speed, callback){
+module.exports = {
+  moveForward: function(drone, time, speed, callback){
     drone.forward(speed);
     setTimeout(function(){
       callback();
     }, time);
-  }
+  },
 
-  this.moveBack = function(drone, time, speed, callback){
+  moveBack: function(drone, time, speed, callback){
     drone.backword(speed);
     setTimeout(function(){
       callback();
     }, time);
-  }
+  },
 
-  this.moveRight = function(drone, time, angle, callback){
+  moveRight: function(drone, time, angle, callback){
     drone.right(angle);
     setTimeout(function(){
       callback();
     }, time);
-  }
+  },
 
-  this.moveLeft = function(drone, time, angle, callback){
+  moveLeft: function(drone, time, angle, callback){
     drone.left(angle);
     setTimeout(function(){
       callback();
